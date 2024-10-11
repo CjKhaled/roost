7ba8bcd4b76bcb3b3b8f338700d3e5fd5860e879
CjKhaled <clauvinsalexander@gmail.com>
2024-10-11 16:50:45 -0400

    feat: create user controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const controller = require('../controllers/userController')
 
-router.get('/:userID')
-router.get('/')
-router.post('/create')
-router.put('/update/:userID')
-router.delete('/:userID')
+router.get('/:userID', controller.getSingleUser)
+router.get('/', controller.getAllUsers)
+router.post('/create', controller.createANewUser)
+router.put('/update/:userID', controller.updateAnExistingUser)
+router.delete('/:userID', controller.deleteAnExistingUser)
 
 module.exports = router
