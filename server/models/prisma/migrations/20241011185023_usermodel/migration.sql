331d98b1b3bca761b87c676054db721180d4da65
CjKhaled <clauvinsalexander@gmail.com>
2024-10-11 14:50:50 -0400

    feat: Created User model

---
+++ server/models/prisma/migrations/20241011185023_usermodel/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[Email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `Email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `FirstName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - The required column `Id` was added to the `User` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+  - Added the required column `LastName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `Password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "Email" VARCHAR(50) NOT NULL,
+ADD COLUMN     "FirstName" VARCHAR(30) NOT NULL,
+ADD COLUMN     "Id" TEXT NOT NULL,
+ADD COLUMN     "LastName" VARCHAR(30) NOT NULL,
+ADD COLUMN     "Password" TEXT NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("Id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_Email_key" ON "User"("Email");
