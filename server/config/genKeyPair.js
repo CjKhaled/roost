77d0867896316daa520dc9e06a455b0107906102
CjKhaled <clauvinsalexander@gmail.com>
2024-10-12 00:45:58 -0400

    feat: create key pair generation

---
+++ server/config/genKeyPair.js
@@ -0,0 +1,31 @@
+const crypto = require('crypto')
+const fs = require('fs')
+const path = require('path')
+
+function genKeyPair () {
+  const keyPair = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+      type: 'pkcs1',
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs1',
+      format: 'pem'
+    }
+  })
+
+  const keysDir = path.join(__dirname, 'keys')
+
+  if (!fs.existsSync(keysDir)) {
+    fs.mkdirSync(keysDir)
+  }
+
+  const publicKeyPath = path.join(keysDir, 'public.key')
+  const privateKeyPath = path.join(keysDir, 'private.key')
+
+  fs.writeFileSync(publicKeyPath, keyPair.publicKey)
+  fs.writeFileSync(privateKeyPath, keyPair.privateKey)
+}
+
+module.exports = genKeyPair
--- server/config/hash.js
+++ server/config/hash.js
@@ -1,25 +1,25 @@
 const bcrypt = require('bcryptjs')
 const AppError = require('./AppError')
 
-async function hashPassword(password) {
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10)
-        return hashedPassword
-    } catch (error) {
-        throw new AppError('Error hashing password', 500)
-    }
+async function hashPassword (password) {
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10)
+    return hashedPassword
+  } catch (error) {
+    throw new AppError('Error hashing password', 500)
+  }
 }
 
-async function compareHashes(givenPassword, storedPassword) {
-    try {
-        const result = await bcrypt.compare(givenPassword, storedPassword)
-        return result
-    } catch {
-        throw new AppError('Error comparing hashes', 500)
-    }
+async function compareHashes (givenPassword, storedPassword) {
+  try {
+    const result = await bcrypt.compare(givenPassword, storedPassword)
+    return result
+  } catch {
+    throw new AppError('Error comparing hashes', 500)
+  }
 }
 
 module.exports = {
-    hashPassword,
-    compareHashes
+  hashPassword,
+  compareHashes
 }
