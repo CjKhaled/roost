95785f2e7f844fe289af79eb3dd4ad742a9ba869
CjKhaled <clauvinsalexander@gmail.com>
2024-10-11 20:49:34 -0400

    Create custom errorhandler

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const errorHandler = require('./middleware/errorHandler')
 const userRouter = require('./routes/userRoutes')
 
 app.use('/api/users', userRouter)
+app.use(errorHandler)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`server listening on port ${port}`))
