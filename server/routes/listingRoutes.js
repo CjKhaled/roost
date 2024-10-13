776b438ca825df51a5179aa16bd2fb8f6162b585
xcao2005 <xcao2005@gmail.com>
2024-10-13 14:41:13 -0400

    debug syntax issue

--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -4,13 +4,8 @@ const controller = require('../controllers/listingController')
 const validator = require('../validations/listingValidations')
 
 router.get('/:listingID', controller.getSingleListing)
-
 router.get('/', controller.getAllListingsController)
-
-router.post('/create', validator.listingCreationValidation, controller.createANewListing)
-
-router.put('/update/:listingID', validator.listingUpdateValidation, controller.updateAnExistingListing)
-
+router.post('/create', validator.validateListingCreate, controller.createANewListing)
+router.put('/update/:listingID', validator.validateListingUpdate, controller.updateAnExistingListing)
 router.delete('/:listingID', controller.deleteAnExistingListing)
-
 module.exports = router
