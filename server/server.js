edf40288efd07e95b63a53a549af41cb35f21861
CjKhaled <clauvinsalexander@gmail.com>
2024-10-31 13:58:31 -0400

    fix: cors issue

--- server/server.js
+++ server/server.js
@@ -2,9 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const passport = require('passport')
 const errorHandler = require('./middleware/errorHandler')
 
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
