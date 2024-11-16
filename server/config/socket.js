4f7453129eb408a6c86f69ef3956b8b24d00ffb0
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2024-11-15 23:41:45 -0500

    use messae service on socket messages

--- server/config/socket.js
+++ server/config/socket.js
@@ -1,4 +1,12 @@
 const socketIO = require('socket.io');
+const {
+  getAllConversations,
+  getMessages,
+  getOrCreateConversation,
+  sendMessage,
+  markMessageAsRead,
+  getUnreadMessagesCount,
+} = require('../services/messageService');
 
 class SocketService {
   constructor() {
@@ -19,25 +27,58 @@ class SocketService {
 
       socket.on('userOnline', (userId) => {
         console.log(`User online: ${userId}`)
+        socket.join(userId)
       })
 
       socket.on('typing', (data) => {
-        socket.to(data.recipientId).emit('typing', data);
+        console.log("Typing: ", data);
+        const { recipientId, username } = data;
+        socket.to(recipientId).emit('typing', { username });
       })
 
       socket.on('stopTyping', (data) => {
-        socket.to(data.recipientId).emit('stopTyping', data);
+        console.log("Stop typing: ", data);
+        const { recipientId } = data;
+        socket.to(recipientId).emit('stopTyping');
       })
 
-      socket.on('sendMessage', (data) => {
-        // Save message to database
+      socket.on('sendMessage', async (data) => { // could make it syncronous but it complicates handling failures
         console.log("Message sent: ", data);
-        socket.to(data.senderId).emit('recieveMessage', data);
+        try {
+          const { senderId, receiverId, conversationId, content } = data;
+          const message = await sendMessage(senderId, receiverId, conversationId, content);
+          console.log("Message sent: ", message);
+
+          socket.to(receiverId).emit('receiveMessage', message);
+        } catch (error) {
+          console.error('Error sending message:', error.message);
+          socket.emit('error', { message: 'Could not send message' });
+        }
       })
 
-      socket.on('readMessage', (data) => {
-        socket.to(data.senderId).emit('messageRead', data);
+      socket.on('readMessage', async (data) => {
+        console.log("Message read: ", data);
+        try {
+          const { messageId } = data;
+          const message = await markMessageAsRead(messageId);
+
+          socket.to(message.senderId).emit('messageRead', message);
+        } catch (error) {
+          console.error('Error marking message as read:', error.message);
+          socket.emit('error', { message: 'Could not mark message as read' });
+        }
       })
+
+      socket.on('getMessages', async (conversationId) => {
+        console.log("Get messages: ", conversationId);
+        try {
+          const messages = await getMessages(conversationId);
+          socket.emit('messages', messages);
+        } catch (error) {
+          console.error('Error retrieving messages:', error.message);
+          socket.emit('error', { message: 'Could not retrieve messages' });
+        }
+      });
 
       socket.on('disconnect', () => {
         console.log(`User disconnected: ${socket.id}`);
