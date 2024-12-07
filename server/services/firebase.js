97276ccf7446b16c21f8038d145548d7f349cf3d
CjKhaled <clauvinsalexander@gmail.com>
2024-12-06 20:24:45 -0500

    fix deployment issue

--- server/services/firebase.js
+++ server/services/firebase.js
@@ -9,12 +9,12 @@ const { initializeApp } = require('firebase/app')
 require('dotenv').config()
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 initializeApp(firebaseConfig)
