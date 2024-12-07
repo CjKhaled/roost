7b06b558c776f8ce5e06f9c1d3df673c7aed05fe
CjKhaled <clauvinsalexander@gmail.com>
2024-12-06 19:52:50 -0500

    fix deploy issue

--- deploy/compute.tf
+++ deploy/compute.tf
@@ -6,6 +6,17 @@ resource "aws_instance" "ec2-instance" {
   subnet_id                   = aws_subnet.public-subnet.id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.security-group.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y docker
+              service docker start
+              usermod -a -G docker ec2-user
+              yum install -y aws-cli
+              EOF
+
+              
   tags = {
     Name = var.ec2-instance-name
   }
